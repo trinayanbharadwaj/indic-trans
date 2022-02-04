a284b6d35120e4f9773b3a1aedf86f69d2e2c813
Kumar Trinayan Bharadwaj <trinayanbharadwaj@gmail.com>
2022-02-04 17:34:24 +0530

    Delete indictrans/_decode directory

--- indictrans/_decode/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright Irshad Ahmad Bhat 2016.
-
-"""Decoding (inference) algorithms."""
-
-from indictrans._decode import viterbi
-from indictrans._decode import beamsearch
-
-DECODERS = {"viterbi": viterbi,
-            "beamsearch": beamsearch}
